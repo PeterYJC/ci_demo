7376556c1416edbd6ea87dca85619faa838f4313
peteryjc <yjcpeter@gmail.com>
2019-12-01 20:50:33 +0800

    first commmit

---
+++ helpers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-
+""" 
+@author: peteryjc
+@mail: yjcpeter@gmail.com
+@file: helpers.py 
+@time: 2019/12/01
+"""
+
+import socket
+
+def communicate(host, port, request):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.send(request)
+    response = s.recv(1024)
+    s.close()
+    return response
---
+++ test/test_pass.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-
+""" 
+@author: peteryjc
+@license: Apache Licence 
+@file: test_pass.py 
+@time: 2019/12/01
+
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛ 
+"""
+import unittest
+
+class TestFileFail(unittest.TestCase):
+
+    def test_pass(self):
+        self.assertTrue("Fail")
